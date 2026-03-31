9af8beb9f4fdbf3dcf7eaf0f1761a8f86edaefe8
Trung Quang Nguyễn <110911150+trung08052001-hcm@users.noreply.github.com>
2026-03-31 23:18:24 +0700

    update code

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect("mongodb://localhost:27017/");
         console.log('MongoDB connected');
     } catch (err) {
         console.error('MongoDB connection error:', err);
