1bb348eed6a591d5eb607f2aaf869cb28837cd0c
Trung Quang Nguyễn <110911150+trung08052001-hcm@users.noreply.github.com>
2026-04-04 00:26:25 +0700

    update code

---
+++ gen.sh
@@ -0,0 +1,2 @@
+npm install firebase-admin
+npm run start
--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/");
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('MongoDB connected');
     } catch (err) {
         console.error('MongoDB connection error:', err);
